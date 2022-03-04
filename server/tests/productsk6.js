30ed4b398c0d0e22bf427c7f4ae3207ccc7f2d6c
sotoi <sotoi@uci.edu>
2022-03-04 14:00:46 -0800

    finished setting up k6

---
+++ server/tests/productsk6.js
@@ -0,0 +1,24 @@
+import http from 'k6/http';
+import { sleep, check } from 'k6';
+import {Counter} from 'k6/metrics';
+export const request = new Counter('http_reqs')
+export const options = {
+  vus:100,
+  duration: '15s',
+
+};
+ let count = 0;
+export default function () {
+  count++
+  const res = http.get(`http://localhost:3000/api/fec2/hr-lax/products/${count}`)
+
+  check(res, {
+    'status was 200': (r) => r.status == 200,
+    'transaction time< 200ms': r => r.timings.duration <200,
+    'transaction time< 500ms': r => r.timings.duration <500,
+    'transaction time< 1000ms': r => r.timings.duration <1000,
+    'transaction time< 2000ms': r => r.timings.duration <2000,
+  });
+  // sleep(1);
+
+}
