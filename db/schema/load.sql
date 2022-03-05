870a32edf45a149fa8a247d8336b7694140471fe
sotoi <sotoi@uci.edu>
2022-03-04 17:44:48 -0800

    server ready

--- db/schema/load.sql
+++ db/schema/load.sql
@@ -1,23 +1,37 @@
-\c products;
-COPY products(id, name, slogan, description, category, default_price) FROM '/mnt/c/Users/irvin/Desktop/data/product.csv' DELIMITER ',' CSV HEADER;
-COPY features(feature_id, product_id, feature, value) FROM '/mnt/c/Users/irvin/Desktop/data/features.csv' DELIMITER ',' CSV HEADER;
-COPY styles(style_id, productId, name, sale_price, original_price, default_style) FROM '/mnt/c/Users/irvin/Desktop/data/styles.csv' DELIMITER ',' CSV HEADER;
-COPY photos(photo_id, styleId, url, thumbnail_url) FROM '/mnt/c/Users/irvin/Desktop/data/photos.csv' DELIMITER ',' CSV HEADER;
-COPY skus(sku_id, styleId, size, quantity) FROM '/mnt/c/Users/irvin/Desktop/data/skus.csv' DELIMITER ',' CSV HEADER;
-COPY related(related_id, current_product_id, related_product_id) FROM '/mnt/c/Users/irvin/Desktop/data/related.csv' DELIMITER ',' CSV HEADER;
+-- \c products;
+-- COPY products(id, name, slogan, description, category, default_price) FROM '/mnt/c/Users/irvin/Desktop/data/product.csv' DELIMITER ',' CSV HEADER;
+-- COPY features(feature_id, product_id, feature, value) FROM '/mnt/c/Users/irvin/Desktop/data/features.csv' DELIMITER ',' CSV HEADER;
+-- COPY styles(style_id, productId, name, sale_price, original_price, default_style) FROM '/mnt/c/Users/irvin/Desktop/data/styles.csv' DELIMITER ',' CSV HEADER;
+-- COPY photos(photo_id, styleId, url, thumbnail_url) FROM '/mnt/c/Users/irvin/Desktop/data/photos.csv' DELIMITER ',' CSV HEADER;
+-- COPY skus(sku_id, styleId, size, quantity) FROM '/mnt/c/Users/irvin/Desktop/data/skus.csv' DELIMITER ',' CSV HEADER;
+-- COPY related(related_id, current_product_id, related_product_id) FROM '/mnt/c/Users/irvin/Desktop/data/related.csv' DELIMITER ',' CSV HEADER;
 
 
 
 -- \c products;
+-- -- \set localpath `pwd`'/data/product.csv'
+-- \copy products(id, name, slogan, description, category, default_price) FROM '/mnt/c/Users/irvin/Desktop/data/product.csv' DELIMITER ',' CSV HEADER;
+-- -- \set localpath `pwd`'/data/features.csv'
+-- \copy features(feature_id, product_id, feature, value) FROM '/mnt/c/Users/irvin/Desktop/data/features.csv' DELIMITER ',' CSV HEADER;
+-- -- \set localpath `pwd`'/data/styles.csv'
+-- \copy styles(style_id, productId, name, sale_price, original_price, default_style) FROM '/mnt/c/Users/irvin/Desktop/data/styles.csv' DELIMITER ',' CSV HEADER;
+-- -- \set localpath `pwd`'/data/photos.csv'
+-- \copy photos(photo_id, styleId, url, thumbnail_url) FROM '/mnt/c/Users/irvin/Desktop/data/photos.csv' DELIMITER ',' CSV HEADER;
+-- -- \set localpath `pwd`'/data/skus.csv'
+-- \copy skus(sku_id, styleId, size, quantity) FROM '/mnt/c/Users/irvin/Desktop/data/skus.csv' DELIMITER ',' CSV HEADER;
+-- -- \set localpath `pwd`'/data/related.csv'
+-- \copy related(related_id, current_product_id, related_product_id) FROM '/mnt/c/Users/irvin/Desktop/data/related.csv' DELIMITER ',' CSV HEADER;
+
+
 -- \set localpath `pwd`'/data/product.csv'
--- COPY products(id, name, slogan, description, category, default_price) FROM :'localpath' DELIMITER ',' CSV HEADER;
+\copy products(id, name, slogan, description, category, default_price) FROM '/home/ubuntu/product-api/data/product.csv' DELIMITER ',' CSV HEADER;
 -- \set localpath `pwd`'/data/features.csv'
--- COPY features(feature_id, product_id, feature, value) FROM :'localpath' DELIMITER ',' CSV HEADER;
+\copy features(feature_id, product_id, feature, value) FROM '/home/ubuntu/product-api/data/features.csv' DELIMITER ',' CSV HEADER;
 -- \set localpath `pwd`'/data/styles.csv'
--- COPY styles(style_id, productId, name, sale_price, original_price, default_style) FROM :'localpath' DELIMITER ',' CSV HEADER;
+\copy styles(style_id, productId, name, sale_price, original_price, default_style) FROM '/home/ubuntu/product-api/data/styles.csv' DELIMITER ',' CSV HEADER;
 -- \set localpath `pwd`'/data/photos.csv'
--- COPY photos(photo_id, styleId, url, thumbnail_url) FROM :'localpath' DELIMITER ',' CSV HEADER;
+\copy photos(photo_id, styleId, url, thumbnail_url) FROM '/home/ubuntu/product-api/data/photos.csv' DELIMITER ',' CSV HEADER;
 -- \set localpath `pwd`'/data/skus.csv'
--- COPY skus(sku_id, styleId, size, quantity) FROM :'localpath' DELIMITER ',' CSV HEADER;
+\copy skus(sku_id, styleId, size, quantity) FROM '/home/ubuntu/product-api/data/skus.csv' DELIMITER ',' CSV HEADER;
 -- \set localpath `pwd`'/data/related.csv'
--- COPY related(related_id, current_product_id, related_product_id) FROM :'localpath' DELIMITER ',' CSV HEADER;
+\copy related(related_id, current_product_id, related_product_id) FROM '/home/ubuntu/product-api/data/related.csv' DELIMITER ',' CSV HEADER;
